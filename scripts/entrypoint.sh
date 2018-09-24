355cfd16cce0fbcb03765f1bcbdf0b38514f93b2
Lukas M <lukas.mrtvy@gmail.com>
2018-09-24 17:12:12 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,6 +8,8 @@ if [ "${USE_SSL,,}" = true ]; then
     sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 fi
 
+sed -i 's/logfile=\/var\/webmin\/miniserv.log/logfile=\/dev\/stdout/g' /etc/webmin/miniserv.conf
+sed -i 's/errorlog=\/var\/webmin\/miniserv.error/errorlog=\/dev\/stderr/g' /etc/webmin/miniserv.conf
 
 echo root:${WEBMIN_PASS} | chpasswd
 
