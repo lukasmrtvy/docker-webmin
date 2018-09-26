cfcd86061cc1aec729d74097540aa9639592b1db
Lukas M <lukas.mrtvy@gmail.com>
2018-09-26 15:15:18 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,12 +3,10 @@
 export WEBMIN_PASS="${WEBMIN_PASS:-admin}"
 export USE_SSL="${USE_SSL:-true}"
 export BASE_URL="${BASE_URL:-localhost}"
+export DISABLE_OTHER_MODULES="${DISABLE_OTHER_MODULES:-true}"
 
 export SD_USER="${SD_USER:-admin}"
 export SD_PASS="${SD_PASS:-admin}"
-
-
-
 
 if [ "${USE_SSL,,}" = true ] && [ -n "${BASE_URL+x}" ]; then
     echo "Generating SSL certificate"
@@ -20,27 +18,13 @@ if [ "${USE_SSL,,}" = true ] && [ -n "${BASE_URL+x}" ]; then
     rm -rf $tempdir
 fi
 
-#export DISABLED_MODULES=(fail2ban)
-
-#for i in ${DISABLED_MODULES} do
-#    sed -e 's/"${i}"//' -i /etc/webmin/webmin.acl
-#do
-
-echo "admin: samba system-status" >  /etc/webmin/webmin.acl
-
-
-
-#kfile=$config_dir/miniserv.pem
-#openssl req -newkey rsa:2048 -x509 -nodes -out $tempdir/cert -keyout $tempdir/key -days 1825 -sha256 >/dev/null 2>&1 
-#cat $tempdir/cert $tempdir/key >$kfile
-
-#sed -i 's/logfile=\/var\/webmin\/miniserv.log/logfile=\/dev\/stdout/g' /etc/webmin/miniserv.conf
-#sed -i 's/errorlog=\/var\/webmin\/miniserv.error/errorlog=\/dev\/stderr/g' /etc/webmin/miniserv.conf
+if [ "${DISABLE_OTHER_MODULES,,}" = true ]; then
+    echo "admin: samba system-status" >  /etc/webmin/webmin.acl
+fi
 
 if [ ! "${WEBMIN_PASS}" = "admin" ];then
     echo "Changing password for admin"
     /opt/webmin/changepass.pl /etc/webmin admin ${WEBMIN_PASS}
-#echo root:${WEBMIN_PASS} | chpasswd
 fi
 
 exec "$@"
