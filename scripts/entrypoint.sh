ad7923fa4bcb5529492fc06868c9e58a340831e0
Lukas M <lukas.mrtvy@gmail.com>
2019-02-10 15:46:11 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-export WEBMIN_PASS="${WEBMIN_PASS:-admin}"
+export WEBMIN_PASSWORD="${WEBMIN_PASSWORD:-admin}"
 export USE_SSL="${USE_SSL:-true}"
 export BASE_URL="${BASE_URL:-localhost}"
-export DISABLE_OTHER_MODULES="${DISABLE_OTHER_MODULES:-true}"
+export ALLOW_ONLY_SAMBA_RELATED_MODULES="${ALLOW_ONLY_SAMBA_RELATED_MODULES:-true}"
 
 if [ "${USE_SSL,,}" = true ] && [ -n "${BASE_URL+x}" ]; then
-    echo "Generating SSL certificate"
     sed -i 's/ssl=/ssl=1/g' /etc/webmin/miniserv.conf
-    tempdir=/tmp/certs
-    mkdir $tempdir
-    openssl req -newkey rsa:2048 -x509 -nodes -out $tempdir/cert -keyout $tempdir/key -days 1825 -sha256 -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=${BASE_URL}" || exit 1
-    cat $tempdir/cert $tempdir/key > /etc/webmin/miniserv.pem
-    rm -rf $tempdir
+    if [ ! -f /etc/webmin/miniserv.pem ]; then
+        echo "Generating SSL certificate"
+        tempdir=/tmp/certs
+        mkdir -p $tempdir
+        openssl req -newkey rsa:2048 -x509 -nodes -out $tempdir/cert -keyout $tempdir/key -days 1825 -sha256 -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=${BASE_URL}" || exit 1
+        cat $tempdir/cert $tempdir/key > /etc/webmin/miniserv.pem
+        rm -rf $tempdir
+    fi
 fi
 
-if [ "${DISABLE_OTHER_MODULES,,}" = true ]; then
+if [ "${ALLOW_ONLY_SAMBA_RELATED_MODULES,,}" = true ]; then
     echo "admin: samba system-status backup-config changeuser webminlog webmin acl mount" >  /etc/webmin/webmin.acl
 fi
 
-if [ ! "${WEBMIN_PASS}" = "admin" ];then
+if [ ! "${WEBMIN_PASSWORD}" = "admin" ];then
     echo "Changing password for admin"
     /opt/webmin/changepass.pl /etc/webmin admin ${WEBMIN_PASS}
 fi
